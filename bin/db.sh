d1631f565786494e145e0df9db1c4720d5d3988e
Herbert Verdida <38489033+bertdida@users.noreply.github.com>
2024-01-14 21:43:49 +0800

    fix: disable db:create

--- bin/db.sh
+++ bin/db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-npx sequelize-cli db:create
+# https://help.heroku.com/63D7ALXT/why-am-i-seeing-user-does-not-have-connect-privilege-error-with-heroku-postgres-on-review-apps
+# npx sequelize-cli db:create
+
 npx sequelize-cli db:migrate
